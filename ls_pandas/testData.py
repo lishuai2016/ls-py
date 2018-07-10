3210bb10a86f34f180bbcc57842571af793f82d8
lishuai80 <lishuai29@jd.com>
2018-07-10 19:55:44 +0800

    Python数据分析数据1

--- ls_pandas/testData.py
+++ ls_pandas/testData.py
@@ -1,7 +1,27 @@
 from pandas import Series,DataFrame
 import pandas as  pd
 
+#读取当前目录下的文件
+# file = open(r'ch06\ex1.csv', encoding='utf-8')
+# print(file)
 
-df = pd.read_csv('C:\Users\lishuai29\Desktop\python\pydata-book-master\ch06\ex1.cvs')
-print(df)
+#普通方法读取：
+# with open(r'ch06\ex1.csv') as file:
+#     for line in  file:
+#         print(line)
+#
+# #用CSV标准库读取：
+# import csv
+# csv_reader = csv.reader(open(r'ch06\ex1.csv'))
+# for row in csv_reader:
+#     print(row)
+#
+# #用pandas读取：
+# data = pd.read_csv(r'ch06\ex1.csv')  #获得一个dataframe对象
+# print(data)
 
+# data1 = pd.read_table(r'ch06\ex6.csv',',')  #指定逗号分隔符
+# print(data1)
+
+data = pd.read_csv(r'ch06\ex6.csv',nrows = 5)  #获得一个dataframe对象
+print(data)
